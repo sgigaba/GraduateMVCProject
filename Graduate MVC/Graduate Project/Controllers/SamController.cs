3c565058643759e8fdb03a980c521d9719708264
Samkelo Gigaba <sgigaba@student.wethinkcode.co.za>
2023-03-06 11:32:14 +0200

    First commit of MVC project

---
+++ Graduate MVC/Graduate Project/Controllers/SamController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Graduate_Project.Controllers
+{
+    public class SamController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
